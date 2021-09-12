a06beee7a93063d959de098896ce8b1024b6880e
ob-ivan <ob-ivan@users.noreply.github.com>
2021-09-12 19:01:37 +0200

    Add the Maze component to the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import {Maze} from "./maze/Maze";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <Maze maxX={80} maxY={25}/>
       </header>
     </div>
   );
