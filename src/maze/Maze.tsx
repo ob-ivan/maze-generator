8f027665fcba8db8c1c6395918b79d211befb55e
ob-ivan <ob-ivan@users.noreply.github.com>
2021-09-12 19:35:15 +0200

    Add a primitive wall removing algorithm

--- src/maze/Maze.tsx
+++ src/maze/Maze.tsx
@@ -11,6 +11,8 @@ interface MazeState {
     cells: Cell[][];
 }
 
+type Direction = 'up' | 'right' | 'down' | 'left';
+
 export class Maze extends React.Component<MazeProps, MazeState> {
     constructor(props: MazeProps) {
         super(props);
@@ -20,6 +22,36 @@ export class Maze extends React.Component<MazeProps, MazeState> {
             cells[y] = [];
             for (let x = 0; x < this.props.maxX; ++x) {
                 cells[y][x] = new Cell();
+            }
+        }
+
+        for (let i = 0, maxI = this.props.maxX * this.props.maxY; i < maxI; ++i) {
+            let x = Math.floor(Math.random() * this.props.maxX);
+            let y = Math.floor(Math.random() * this.props.maxY);
+            let directions: { d: Direction, dx: number, dy: number }[] = [
+                {d: 'up', dx: -1, dy: 0},
+                {d: 'right', dx: 0, dy: 1},
+                {d: 'down', dx: 1, dy: 0},
+                {d: 'left', dx: 0, dy: -1},
+            ];
+            let neighbours: { d: Direction, c: Cell }[] = [];
+            directions.forEach(({ d, dx, dy }) => {
+                let nx = x + dx;
+                let ny = y + dy;
+                if (
+                    (0 <= nx) && (nx < this.props.maxX) &&
+                    (0 <= ny) && (ny < this.props.maxY)
+                ) {
+                    neighbours.push({ d, c: cells[ny][nx]});
+                }
+            });
+            let neighbour = neighbours[Math.floor(Math.random() * neighbours.length)];
+            let cell = cells[y][x];
+            switch (neighbour.d) {
+                case "up": cell.removeWallUp(); neighbour.c.removeWallDown(); break;
+                case "right": cell.removeWallRight(); neighbour.c.removeWallLeft(); break;
+                case "down": cell.removeWallDown(); neighbour.c.removeWallUp(); break;
+                case "left": cell.removeWallLeft(); neighbour.c.removeWallRight(); break;
             }
         }
 
