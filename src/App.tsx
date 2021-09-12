be0bf8ef9848c6f0ca46579727c2faad3d48b15f
ob-ivan <ob-ivan@users.noreply.github.com>
2021-09-12 20:12:49 +0200

    DEBUG: Add pivot cell numbers

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <Maze maxX={20} maxY={15}/>
+          <Maze maxX={25} maxY={20}/>
       </header>
     </div>
   );
